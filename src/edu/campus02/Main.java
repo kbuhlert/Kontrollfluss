95adee6d59b27fd0fb56ea43c084c7fbcbc7adad
kbuhlert <kbuhlert@gmail.com>
2020-03-05 09:05:07 +0100

    erste Bedingung im Code

--- src/edu/campus02/Main.java
+++ src/edu/campus02/Main.java
@@ -3,6 +3,13 @@ package edu.campus02;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int ersterWert = 1; // write your code here
+    int zweiterWert = 2;
+     boolean bedingung = ersterWert<zweiterWert;
+
+     if(bedingung){
+         System.out.println("Die Begingung ist wahr, erster Wert ist kleiner zweiter Wert");
+     } else {
+            System.out.println("Das ist nicht der Fall");}
     }
 }
